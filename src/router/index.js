82255db5a44391834538e3e00b8adb07abe426f3
saltedfish <huyashangding@163.com>
2021-03-14 17:44:23 +0800

    finish header

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Loading from '@/components/loading'
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/loading',
+    name: 'Loading',
+    component: Loading
   }
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,216 @@
 <template>
   <div class="home">
-    <container :options="{ width: 3840, height: 2160 }">
-      <div>111</div>
+    <loading v-if="load">
+      <div>数据大屏加载中...</div>
+    </loading>
+    <container :options="{ width: 3840, height: 2160 }" v-else>
+      <div class="header">
+        <top-header></top-header>
+      </div>
+      <div class="separator">222</div>
+      <div class="center">
+        <div class="left">
+          <div class="left1">3333</div>
+          <div class="left2">3333</div>
+          <div class="left3">3333</div>
+          <div class="left4">3333</div>
+          <div class="left5">3333</div>
+          <div class="left6">3333</div>
+        </div>
+        <div class="right">
+          <div class="right-top1">333</div>
+          <div class="right-top2">333</div>
+          <div class="right-bottom">
+            <div class="right-left">
+              <div class="right-left1">333</div>
+              <div class="right-left2">333</div>
+              <div class="right-left3">333</div>
+            </div>
+            <div class="right-right">
+              <div class="right-right1">333</div>
+              <div class="right-right2">333</div>
+              <div class="right-right3">333</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Loading from '@/components/loading/index'
 import Container from '@/components/container/Container'
+import TopHeader from '@/components/TopHeader/index'
+import { ref, onMounted } from 'vue'
+
 export default {
   name: 'Home',
   components: {
-    Container
+    Container,
+    Loading,
+    TopHeader
+  },
+  setup () {
+    const load = ref(true)
+
+    onMounted(() => {
+      setTimeout(() => {
+        load.value = false
+      }, 2500)
+    })
+
+    return {
+      load
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 .home
-  color red
-  font-size 30px
+  width 100%
+  height 100%
+  background rgb(29, 29, 29)
+  color #fff
+  font-size 48px
+  #container {
+    display: flex;
+    flex-direction: column;
+
+    .header {
+      height: 167px;
+      margin-top: 10px;
+    }
+
+    .separator-wrapper {
+      height: 10px;
+    }
+
+    .center {
+      flex: 1;
+      display: flex;
+
+      .left {
+        flex: 0 0 860px;
+        display: flex;
+        flex-direction: column;
+        width: 860px;
+        height: 100%;
+        margin: 0 10px;
+        box-sizing: border-box;
+        background: rgb(60, 61, 64);
+
+        .left1, .left2, .left3, .left4, .left5, .left6 {
+          padding-bottom: 20px;
+        }
+
+        .left1 {
+          height: 300px;
+        }
+
+        .left2 {
+          height: 320px;
+        }
+
+        .left3 {
+          height: 280px;
+        }
+
+        .left4 {
+          height: 230px;
+        }
+
+        .left5 {
+          height: 360px;
+        }
+
+        .left6 {
+          height: 360px;
+        }
+      }
+
+      .right {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        margin: 0 10px;
+        max-width: 2960px;
+        overflow: hidden;
+
+        .right-top1 {
+          height: 206px;
+          margin-bottom: 20px;
+        }
+
+        .right-top2 {
+          height: 48px;
+          margin-bottom: 20px;
+        }
+
+        .right-bottom {
+          flex: 1;
+          display: flex;
+
+          .right-left {
+            display: flex;
+            flex-direction: column;
+            width: 1917px;
+
+            .right-left1 {
+              height: 999px;
+            }
+
+            .right-left2 {
+              height: 80px;
+              padding-top: 20px;
+              box-sizing: border-box;
+            }
+
+            .right-left3 {
+              height: 350px;
+              margin-top: 10px;
+
+              .dv-border-box-8 {
+                width: 100%;
+                height: 100%;
+                padding: 5px;
+                box-sizing: border-box;
+                background: rgb(66, 68, 70);
+                border-radius: 10px;
+                box-shadow: 10px 10px 10px rgba(0, 0, 0, .3);
+              }
+            }
+
+            .right-left4 {
+              height: 220px;
+              margin-top: 10px;
+            }
+          }
+
+          .right-right {
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            margin-left: 10px;
+
+            .right-right1 {
+              width: 100%;
+              height: 999px;
+              padding-right: 10px;
+              box-sizing: border-box;
+            }
+
+            .right-right2 {
+              width: 100%;
+              height: 650px;
+              margin-top: 20px;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
