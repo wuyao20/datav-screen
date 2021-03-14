d73f3238ee2af2076dfd7468b693bfe197dbf7b6
saltedfish <huyashangding@163.com>
2021-03-14 15:52:17 +0800

    install echarts vue-echarts

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Echarts from 'vue-echarts'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .component('vue-echarts', Echarts)
+  .mount('#app')
